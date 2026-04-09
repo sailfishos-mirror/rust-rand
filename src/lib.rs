8fb44c1a5c92cf5454b798d6b2c18cecd12839f9
Diggory Hardy <git@dhardy.name>
2026-04-09 15:40:47 +0000

    Deprecate feature log

--- src/lib.rs
+++ src/lib.rs
@@ -66,37 +66,6 @@ extern crate alloc;
 #[cfg(feature = "std")]
 extern crate std;
 
-#[allow(unused)]
-macro_rules! trace { ($($x:tt)*) => (
-    #[cfg(feature = "log")] {
-        log::trace!($($x)*)
-    }
-) }
-#[allow(unused)]
-macro_rules! debug { ($($x:tt)*) => (
-    #[cfg(feature = "log")] {
-        log::debug!($($x)*)
-    }
-) }
-#[allow(unused)]
-macro_rules! info { ($($x:tt)*) => (
-    #[cfg(feature = "log")] {
-        log::info!($($x)*)
-    }
-) }
-#[allow(unused)]
-macro_rules! warn { ($($x:tt)*) => (
-    #[cfg(feature = "log")] {
-        log::warn!($($x)*)
-    }
-) }
-#[allow(unused)]
-macro_rules! error { ($($x:tt)*) => (
-    #[cfg(feature = "log")] {
-        log::error!($($x)*)
-    }
-) }
-
 // Re-export rand_core itself
 pub use rand_core;
 
