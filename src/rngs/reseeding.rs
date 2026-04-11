1aeee9f4c506f9f737c6c37c169ccdc365bfbabf
Diggory Hardy <git@dhardy.name>
2026-04-11 11:15:05 +0100

    Prepare v0.9.3: deprecate feature `log` (#1764)

--- src/rngs/reseeding.rs
+++ src/rngs/reseeding.rs
@@ -214,13 +214,10 @@ where
 
     #[inline(never)]
     fn reseed_and_generate(&mut self, results: &mut <Self as BlockRngCore>::Results) {
-        trace!("Reseeding RNG (periodic reseed)");
-
         let num_bytes = size_of_val(results.as_ref());
 
         if let Err(e) = self.reseed() {
-            warn!("Reseeding RNG failed: {}", e);
-            let _ = e;
+            panic!("Reseeding RNG failed: {e}");
         }
 
         self.bytes_until_reseed = self.threshold - num_bytes as i64;
