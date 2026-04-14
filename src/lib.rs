4df037d8f28281fede050bdfadb784e148d562b7
Neal H. Walfield <neal@walfield.org>
2026-04-14 14:53:04 +0200

    Fix the implementation of TrustedLen.
    Enable the `trusted_len` feature.
    
    Implementing `TrustLen` is now unsafe.  Fix the implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,7 @@
 #![doc(test(attr(allow(unused_variables), deny(warnings))))]
 #![no_std]
 #![cfg_attr(feature = "simd_support", feature(stdsimd))]
+#![cfg_attr(feature = "nightly", feature(trusted_len))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![allow(
     clippy::float_cmp,
